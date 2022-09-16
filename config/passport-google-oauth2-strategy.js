282dcd7be61644a2f3f5c608b915fd966bd3d4db
charusaxena134 <charusaxena134@gmail.com>
2022-09-16 18:03:50 +0530

    sign in with google is done

---
+++ config/passport-google-oauth2-strategy.js
@@ -0,0 +1,53 @@
+const passport = require("passport");
+const googleStrategy = require("passport-google-oauth").OAuth2Strategy;
+
+const crypto = require("crypto");
+
+const User = require("../models/user");
+
+// tell passport to use a new strategy for google login
+passport.use(
+  new googleStrategy(
+    {
+      clientID:
+        "263700653691-u6ifk3o9ospvnlcv49p3somodmtm0fb8.apps.googleusercontent.com",
+      clientSecret: "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",
+      callbackURL: "http://localhost:8000/users/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      // find a user
+      User.findOne({ email: profile.emails[0].value }).exec(function (
+        err,
+        user
+      ) {
+        if (err) {
+          console.log("error in google strategy passport", err);
+          return;
+        }
+        console.log(profile);
+        // if found, set this user as a req.user
+        if (user) {
+          return done(null, user);
+        } else {
+          // if not found, create a user and set this as a req.user
+          User.create(
+            {
+              name: profile.displayName,
+              email: profile.emails[0].value,
+              password: crypto.randomBytes(20).toString("hex"),
+            },
+            function (err, user) {
+              if (err) {
+                console.log("error in google passport-strategy", err);
+                return;
+              }
+              return done(null, user);
+            }
+          );
+        }
+      });
+    }
+  )
+);
+
+module.exports = passport;
--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -19,29 +19,30 @@ module.exports.destroy = async function (req, res) {
   try {
     let post = await Post.findById(req.params.id);
 
-    // if (post.user == req.user.id) {
-    post.remove();
+    if (post.user == req.user.id) {
+      post.remove();
 
-    await Comment.deleteMany({ post: req.params.id });
+      await Comment.deleteMany({ post: req.params.id });
 
-    if (req.xhr) {
-      return res.status(200).json({
-        data: {
-          post_id: req.params.id,
-        },
-        message: "Post deleted",
+      if (req.xhr) {
+        return res.status(200).json({
+          data: {
+            post_id: req.params.id,
+          },
+          message: "Post deleted",
+        });
+      }
+
+      req.flash("success", "Post and associated comments deleted!");
+
+      return res.json(200, {
+        message: "Post and associated comments delete successfully",
+      });
+    } else {
+      res.json(401, {
+        message: "You cannot delete post",
       });
     }
-
-    req.flash("success", "Post and associated comments deleted!");
-
-    return res.json(200, {
-      message: "Post and associated comments delete successfully",
-    });
-    // } else {
-    //   req.flash("error", "You cannot delete this post!");
-    //   return res.redirect("back");
-    // }
   } catch (err) {
     return res.json(500, {
       message: "Internal Server Error",
