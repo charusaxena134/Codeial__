a0e658546478ec144b704ee221244bb8f8a97402
charusaxena134 <charusaxena134@gmail.com>
2022-10-05 12:02:09 +0530

    creating a chat room

--- assets/js/chat_engine(1).js
+++ assets/js/chat_engine(1).js
@@ -1,20 +1,57 @@
-class ChatEngine{
-    constructor(chatBoxId, userEmail){
-        this.chatBox = $(`#${chatBoxId}`);
-        this.userEmail = userEmail;
+class ChatEngine {
+  constructor(chatBoxId, userEmail) {
+    this.chatBox = $(`#${chatBoxId}`);
+    this.userEmail = userEmail;
 
-        this.socket = io.connect('http://localhost:5000');
-
-        if (this.userEmail){
-            this.connectionHandler();
-        }
-
+    this.socket = io.connect("http://localhost:5000", {
+      transport: ["websocket"],
+    });
+    if (this.userEmail) {
+      this.connectionHandler();
     }
+  }
 
+  connectionHandler() {
+    let self = this;
+    this.socket.on("connect", function () {
+      console.log("connection established using sockets...!");
+      self.socket.email("join_room", {
+        user_email: self.userEmail,
+        chatroom: "codeial",
+      });
 
-    connectionHandler(){
-        this.socket.on('connect', function(){
-            console.log('connection established using sockets...!');
+      self.socket.on("user_joined", function (data) {
+        console.log("a user joined", data);
+      });
+    });
+    $("#send-message").click(function () {
+      let msg = $("#chat-message-input").val();
+      if (msg != "") {
+        self.socket.emit("send_message", {
+          message: msg,
+          user_email: self.userEmail,
+          chatroom: "codeial",
         });
-    }
+      }
+    });
+    self.socket.on("receive_message", function (data) {
+      console.log("message recived", data.message);
+
+      let newMessage = $("<li>");
+
+      let messageType = "other-message";
+
+      if (data.user_email == self.userEmail) {
+        messageType = "self-message";
+      }
+      newMessage.append($("<span>"), {
+        html: data.message,
+      });
+      newMessage.append("<sub>", {
+        html: data.user_email,
+      });
+      newMessage.addClass(messageType);
+      $("#chat-message-list").append(newMessage);
+    });
+  }
 }
--- routes/index.js
+++ routes/index.js
@@ -12,21 +12,6 @@ router.use("/comments", require("./comments"));
 router.use("/likes", require("./likes"));
 router.use("/api", require("./api"));
 
-router.get("/", function (req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  ); // If needed
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  ); // If needed
-  res.setHeader("Access-Control-Allow-Credentials", true); // If needed
-
-  res.send("cors problem fixed:)");
-});
-
 // for any further routes, access from here
 // router.use('/routerName', require('./routerfile));
 
