778f77dd6430393ae7c57cf809d6f7c0265d7563
charusaxena134 <charusaxena134@gmail.com>
2022-10-05 10:59:14 +0530

    Creating a chat box

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,33 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
-const homeController = require('../controllers/home_controller');
+const homeController = require("../controllers/home_controller");
 
-console.log('router loaded');
+console.log("router loaded");
 
+router.get("/", homeController.home);
+router.use("/users", require("./users"));
+router.use("/posts", require("./posts"));
+router.use("/comments", require("./comments"));
+router.use("/likes", require("./likes"));
+router.use("/api", require("./api"));
 
-router.get('/', homeController.home);
-router.use('/users', require('./users'));
-router.use('/posts', require('./posts'));
-router.use('/comments', require('./comments'));
+router.get("/", function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  ); // If needed
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  ); // If needed
+  res.setHeader("Access-Control-Allow-Credentials", true); // If needed
 
-
-router.use('/api', require('./api'));
+  res.send("cors problem fixed:)");
+});
 
 // for any further routes, access from here
 // router.use('/routerName', require('./routerfile));
-
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,58 +1,31 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const passport = require("passport");
+const passport = require('passport');
 
-const usersController = require("../controllers/users_controller");
-const { validate } = require("../models/token");
-const User = require("../models/user");
+const usersController = require('../controllers/users_controller');
 
-router.get(
-  "/profile/:id",
-  passport.checkAuthentication,
-  usersController.profile
-);
-router.post(
-  "/update/:id",
-  passport.checkAuthentication,
-  usersController.update
-);
+router.get('/profile/:id', passport.checkAuthentication, usersController.profile);
+router.post('/update/:id', passport.checkAuthentication, usersController.update);
 
-router.get("/sign-up", usersController.signUp);
-router.get("/sign-in", usersController.signIn);
+router.get('/sign-up', usersController.signUp);
+router.get('/sign-in', usersController.signIn);
 
-router.post("/create", usersController.create);
+
+router.post('/create', usersController.create);
 
 // use passport as a middleware to authenticate
-router.post(
-  "/create-session",
-  passport.authenticate("local", { failureRedirect: "/users/sign-in" }),
-  usersController.createSession
-);
+router.post('/create-session', passport.authenticate(
+    'local',
+    {failureRedirect: '/users/sign-in'},
+), usersController.createSession);
 
-router.get("/sign-out", usersController.destroySession);
 
-router.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
-router.get(
-  "/auth/google/callback",
-  passport.authenticate("google", { failureRedirect: "/users/sign-in" }),
-  usersController.createSession
-);
+router.get('/sign-out', usersController.destroySession);
 
-router.post("/", async (req, res) => {
-  try {
-    const { error } = validate(req.body);
-    if (error) {
-      return res.status(400).send(error.details[0].message);
-    }
-    const user = await new User(req.body).save();
-    res.send(user);
-  } catch (error) {
-    res.send("An error occured");
-    console.log(error);
-  }
-});
+
+router.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+router.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/users/sign-in'}), usersController.createSession);
+
+
 
 module.exports = router;
